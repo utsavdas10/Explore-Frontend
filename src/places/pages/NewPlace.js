ed5fa68c63360d63fdc8324611e96d8f2fe60e40
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2023-05-26 00:23:27 +0530

    added state management to forms

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Input from '../../shared/components/FormElements/Input';
+
+import './NewPlace.css';
 
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+  return (
+    <form className="place-form">
+      <Input
+        label="Title"
+        element="input" 
+        type="text" 
+        placeholder="Enter a title"
+        errorText="Please enter a valid title"
+        validators={[]}
+      />
+    </form>
+  )
+  
 };
 
 export default NewPlace;
