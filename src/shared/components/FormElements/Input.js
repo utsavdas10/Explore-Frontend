38e046603d77f0bf19be1ebd4afbbb2904071e4a
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2023-05-26 09:10:23 +0530

    finished add place form

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from "react";
+import React, {useReducer, useEffect} from "react";
 
 import {validate} from "../../util/validators";
 
@@ -29,6 +29,13 @@ const Input = props => {
         isValid: false,
         isTouched: false
     });
+
+    const {id, onInput} = props;
+    const {value, isValid} = inputState;
+
+    useEffect(()=>{
+        onInput(id, value, isValid);
+    }, [id, value, isValid, onInput]);
 
     const changeHandler = event => {
         dispatcher({
