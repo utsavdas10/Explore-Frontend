9aa7e45828bc8d00763bf3f9f8044578b4760570
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2023-06-15 23:56:31 +0530

    authorization and minor fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -6,69 +6,72 @@ import {
   Switch
 } from 'react-router-dom';
 
-import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/components/hooks/auth-hook';
+
 
 const App = () => {
-  const [userId, setUserId] = useState("");
-  const [token, setToken] = useState("");
-
-  const login = useCallback((uid, token) => {
-    setUserId(uid)
-    setToken(token)
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-  }, []);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
-
-  if(token){
+  if (token) {
     routes = (
       <Switch>
-        <Route path="/" exact> <Users /> </Route>
-        <Route path="/places/new" exact> <NewPlace /> </Route>
-        <Route path="/places/:placeId" exact> <UpdatePlace /> </Route>
-        <Route path="/:userId/places" exact> <UserPlaces /> </Route>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
         <Redirect to="/" />
       </Switch>
     );
   } else {
     routes = (
       <Switch>
-        <Route path="/" exact> <Users /> </Route>
-        <Route path="/auth" exact> <Auth /> </Route>
-        <Route path="/:userId/places" exact> <UserPlaces /> </Route>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
         <Redirect to="/auth" />
       </Switch>
     );
   }
 
-
   return (
-    <AuthContext.Provider value={
-      {
+    <AuthContext.Provider
+      value={{
         isLoggedIn: !!token,
-        userId: userId,
         token: token,
-        login: login, 
+        userId: userId,
+        login: login,
         logout: logout
-      }}>
+      }}
+    >
       <Router>
         <MainNavigation />
-        <main>
-          {routes}
-        </main>
+        <main>{routes}</main>
       </Router>
     </AuthContext.Provider>
   );
 };
 
 export default App;
+ 
--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -50,7 +50,6 @@ const NewPlace = () => {
       formData.append('title', formState.inputs.title.value);
       formData.append('description', formState.inputs.description.value);
       formData.append('address', formState.inputs.address.value);
-      formData.append('creator', auth.userId);
       formData.append('image', formState.inputs.image.value);
       console.log(formData);
       const responseData = await sendRequest(
